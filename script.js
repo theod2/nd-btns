ed629d929dd3a9c8334f21bbb6ca80e99d3d149d
theo.drf <41112121+theodrf@users.noreply.github.com>
2023-03-16 19:57:45 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -4,92 +4,92 @@ var observer = new MutationObserver(function(mutations){
       addAnswerbtns();
       observer.disconnect();
     }
-  }) 
+  });
   observer.observe(document.body, { 
     childList: true,
     subtree: true
 });
 
 function addQuestionBtns(){
-    let question_container = document.querySelector('[data-testid="question_container"]')
-    const url = new URL(window.location.href)
-    q_id=url.pathname.split('/')[2]
-    let div = document.createElement('div')
-    div.className = "modbox sg-box sg-box--white sg-box--border-color-gray-20 sg-box--no-shadow sg-box--border-radius sg-box--border sg-box--padding-m sg-box--padding-m-border md:sg-box--padding-m-border lg:sg-box--padding-m-border xl:sg-box--padding-m-border sg-box--border-color-gray-20 md:sg-box--border-color-gray-20 lg:sg-box--border-color-gray-20 xl:sg-box--border-color-gray-20"
-    div.innerHTML = '<img src="https://raw.githubusercontent.com/sugiiiiii/nds-moderaton-ui-ext/main/images/host/shield-black.svg" style="width: 25px;">'
-    question_container.insertAdjacentElement("beforebegin", div)
-    const keys = Object.keys(q_reasons)
+    let question_container = document.querySelector('[data-testid="question_container"]');
+    const url = new URL(window.location.href);
+    q_id=url.pathname.split('/')[2];
+    let div = document.createElement('div');
+    div.className = "modbox sg-box sg-box--white sg-box--border-color-gray-20 sg-box--no-shadow sg-box--border-radius sg-box--border sg-box--padding-m sg-box--padding-m-border md:sg-box--padding-m-border lg:sg-box--padding-m-border xl:sg-box--padding-m-border sg-box--border-color-gray-20 md:sg-box--border-color-gray-20 lg:sg-box--border-color-gray-20 xl:sg-box--border-color-gray-20";
+    div.innerHTML = '<img src="https://raw.githubusercontent.com/theodrf/nd-btns/main/icons/shield-black.svg" style="width: 25px;">';
+    question_container.insertAdjacentElement("beforebegin", div);
+    const keys = Object.keys(q_reasons);
     keys.forEach(key => {
         let btn = document.createElement("BUTTON");
-        btn.id=key
-        btn.className= "sg-button sg-button--s sg-button--solid btns"
-        let t = document.createTextNode(key)
+        btn.id=key;
+        btn.className= "sg-button sg-button--s sg-button--solid btns";
+        let t = document.createTextNode(key);
         btn.appendChild(t);
         div.appendChild(btn);
 
         btn.addEventListener('click', event => {
-            deleteQuestion(q_id, q_reasons[btn.id], div)
+            deleteQuestion(q_id, q_reasons[btn.id], div);
         });
-    })
+    });
 }
 
 function addAnswerbtns(){
-    var answers = document.querySelectorAll("div[id^='answer-']")
+    var answers = document.querySelectorAll("div[id^='answer-']");
     for( i = 0; i < answers.length; i++) {
-        let answer_box = document.querySelectorAll('[data-testid="answer_box"]')[i]
-        answer_id=answers[i].id.split("-")[1]
-        let div = document.createElement('div')
-        div.className = "modbox sg-box sg-box--white sg-box--border-color-gray-20 sg-box--no-shadow sg-box--border-radius sg-box--border sg-box--padding-m sg-box--padding-m-border md:sg-box--padding-m-border lg:sg-box--padding-m-border xl:sg-box--padding-m-border sg-box--border-color-gray-20 md:sg-box--border-color-gray-20 lg:sg-box--border-color-gray-20 xl:sg-box--border-color-gray-20"
-        div.innerHTML = '<img src="https://raw.githubusercontent.com/sugiiiiii/nds-moderaton-ui-ext/main/images/host/shield-black.svg" style="width: 25px;">'
-        answer_box.insertAdjacentElement("beforebegin", div)
-        const keys = Object.keys(a_reasons)
+        let answer_box = document.querySelectorAll('[data-testid="answer_box"]')[i];
+        answer_id=answers[i].id.split("-")[1];
+        let div = document.createElement('div');
+        div.className = "modbox sg-box sg-box--white sg-box--border-color-gray-20 sg-box--no-shadow sg-box--border-radius sg-box--border sg-box--padding-m sg-box--padding-m-border md:sg-box--padding-m-border lg:sg-box--padding-m-border xl:sg-box--padding-m-border sg-box--border-color-gray-20 md:sg-box--border-color-gray-20 lg:sg-box--border-color-gray-20 xl:sg-box--border-color-gray-20";
+        div.innerHTML = '<img src=".https://raw.githubusercontent.com/theodrf/nd-btns/main/icons/shield-black.svg" style="width: 25px;">';
+        answer_box.insertAdjacentElement("beforebegin", div);
+        const keys = Object.keys(a_reasons);
         keys.forEach(key => {
             let btn = document.createElement("BUTTON");
-            btn.id=answer_id
-            btn.className= "sg-button sg-button--s sg-button--solid"
-            let t = document.createTextNode(key)
+            btn.id=answer_id;
+            btn.className= "sg-button sg-button--s sg-button--solid";
+            let t = document.createTextNode(key);
             btn.appendChild(t);
             div.appendChild(btn);
 
             btn.addEventListener('click', event => {
-                deleteAnswer(btn.id, a_reasons[btn.innerHTML], div)
+                deleteAnswer(btn.id, a_reasons[btn.innerHTML], div);
         });
-    })
+    });
     }
 }
 
 function deleteQuestion(id, reason, div){
-    let xhr = new XMLHttpRequest()
-	xhr.withCredentials = true
-	xhr.open('POST', "https://nosdevoirs.fr/api/28/moderation_new/delete_task_content")	
-	xhr.setRequestHeader("Content-Type", "application/json")
+    let xhr = new XMLHttpRequest();
+	xhr.withCredentials = true;
+	xhr.open('POST', "https://nosdevoirs.fr/api/28/moderation_new/delete_task_content");
+	xhr.setRequestHeader("Content-Type", "application/json");
     xhr.onreadystatechange = function() {
         if (xhr.readyState == XMLHttpRequest.DONE) {
             if(JSON.parse(xhr.response).success == true){
-                div.innerHTML = '<p class="succes">Question supprimée avec succès !</p>'
+                div.innerHTML = '<p class="succes">Question supprimée avec succès !</p>';
             } else {
-                alert(JSON.parse(xhr.response).message)
+                alert(JSON.parse(xhr.response).message);
             }
         }
     }
-    xhr.send(`{"model_id":${id},"model_type_id":1,"reason_id":4,"reason":"${reason}","give_warning":false,"take_points":true,"return_points":true,"schema":""}`)
+    xhr.send(`{"model_id":${id},"model_type_id":1,"reason_id":4,"reason":"${reason}","give_warning":false,"take_points":true,"return_points":true,"schema":""}`);
 }
 
 function deleteAnswer(id, reason, div){
-    let xhr = new XMLHttpRequest()
-	xhr.withCredentials = true
-	xhr.open('POST', "https://nosdevoirs.fr/api/28/moderation_new/delete_response_content")	
-	xhr.setRequestHeader("Content-Type", "application/json")
+    let xhr = new XMLHttpRequest();
+	xhr.withCredentials = true;
+	xhr.open('POST', "https://nosdevoirs.fr/api/28/moderation_new/delete_response_content");
+	xhr.setRequestHeader("Content-Type", "application/json");
     xhr.onreadystatechange = function() {
         if (xhr.readyState == XMLHttpRequest.DONE) {
             if(JSON.parse(xhr.response).success == true){
-                div.innerHTML = '<p class="succes">Réponse supprimée avec succès !</p>'
+                div.innerHTML = '<p class="succes">Réponse supprimée avec succès !</p>';
             } else {
-                alert(JSON.parse(xhr.response).message)
+                alert(JSON.parse(xhr.response).message);
             }
         }
     }
-    xhr.send(`{"model_id":${id},"model_type_id":2,"reason_id":23,"reason":"${reason}","give_warning":false,"take_points":true,"schema":"moderation.response.delete.req"}`)
+    xhr.send(`{"model_id":${id},"model_type_id":2,"reason_id":23,"reason":"${reason}","give_warning":false,"take_points":true,"schema":"moderation.response.delete.req"}`);
 }
 
 const q_reasons = {
